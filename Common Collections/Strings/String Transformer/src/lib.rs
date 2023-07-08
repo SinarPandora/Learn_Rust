b082c2f1e391abbd7bd8a5a2201d0471033d9b10
Sinar Pandora <SinarPandora@outlook.com>
2023-07-08 19:58:47 +0800

    Task update

--- Common Collections/Strings/String Transformer/src/lib.rs
+++ Common Collections/Strings/String Transformer/src/lib.rs
@@ -8,9 +8,17 @@ pub mod transformer {
     use super::Command;
 
     // TODO: Complete the function signature!
-    pub fn transformer(input: /* Write type of input */) -> /* Write type of result */ {
+    pub fn transformer(input: Vec<(String, Command)>) -> Vec<String> {
         let mut output = vec![];
-        // TODO: Complete the function body!
+        for (string, cmd) in input {
+            match cmd {
+                Command::Uppercase => output.push(string.to_uppercase()),
+                Command::Trim => output.push(string.trim().to_owned()),
+                Command::Append(times) => output.push(
+                    format!("{}{}", string, "bar".repeat(times))
+                )
+            }
+        }
         output
     }
 }
